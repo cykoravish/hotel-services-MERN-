c84c020c2d71e2bc1943941fe8e2ad5f75a385b6
Ravish Bisht <ravishbisht85@gmail.com>
2024-03-04 17:42:17 +0530

    mern app

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      In the serene ambiance of our hotel, we offer a plethora of services to cater to your every need and celebration. Whether you're planning a memorable birthday party, a romantic anniversary dinner, or any other special occasion, our dedicated team is here to ensure it's an unforgettable experience. From elegant banquet halls to intimate dining settings, we provide the perfect venue for your festivities. Our expert staff will assist you in customizing every detail, from menu selection to décor, to suit your preferences and create lasting memories. Additionally, we offer a range of amenities and accommodations to make your stay comfortable and enjoyable. Experience unparalleled hospitality and impeccable service at our hotel, where every moment is crafted with care and excellence.
+      </p>
+    </section>
+  );
+};
+
+export default About;
